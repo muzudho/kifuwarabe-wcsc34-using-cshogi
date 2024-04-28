2a1515008c741d74c6fabc6494757d67f6d9d8a2
むずでょ <muzudho1@gmail.com>
2024-04-28 15:32:27 +0900

    Create display_helper.py

---
+++ display_helper.py
@@ -0,0 +1,61 @@
+class DisplayHelper():
+
+
+    @staticmethod
+    def pretty_number(number):
+        """数を、アンダースコア区切りの文字列に変換します
+
+        'abc' ３文字なら 'abc' アンダースコア要らない
+        'abcd' ４文字なら 'a_bcd' ２文字目にアンダースコアを追加
+        'abcde' ５文字なら 'ab_cde' ３文字目にアンダースコアを追加
+        'abcdef' ６文字なら 'abc_def' ４文字目にアンダースコアを追加
+        'abcdefg' ７文字なら 'a_bcd_efg' ２文字目にアンダースコアを追加
+        'abcdefgh' ８文字なら 'ab_cde_fgh' ３文字目にアンダースコアを追加
+         ４文字以降、２、３、４でループしてそうだ
+        """
+
+        text = str(number)
+        text_len = len(text)
+        #print(f"text:'{text}'  len(text):{len(text)}")
+
+        # １文字ずつに分解
+        characters = list(text)
+
+
+        #print(f"characters len:{len(characters)}")
+        new_text = []
+
+        # 4文字未満なら区切り要らない
+        if text_len < 4:
+            return text
+
+        # アンダースコア挿入の次位置を指すカウンター target を用意する
+        mod3 = (text_len - 4) % 3
+
+        if mod3 == 0:
+            target = 1
+        elif mod3 == 1:
+            target = 2
+        else:
+            target = 3
+
+        # ループカウンタ―
+        i = 0
+        for char in characters:
+
+            # アンダースコア挿入位置に来たら
+            if i == target:
+                new_text.append("_")
+                target += 3
+
+            new_text.append(char)
+            i += 1
+
+        return ''.join(new_text)
+
+
+if __name__ == '__main__':
+    """コマンドから実行時"""
+
+    for i in [1_000_000_000, 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000]:
+        print(f"{i} --> {DisplayHelper.pretty_number(i)}")
