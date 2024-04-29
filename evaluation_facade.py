0c8285f69d31074003886a6b1af38fc575020f29
むずでょ <muzudho1@gmail.com>
2024-04-29 23:24:43 +0900

    WIP mm

--- evaluation_facade.py
+++ evaluation_facade.py
@@ -11,6 +11,7 @@ from evaluation_table_facade_pp import EvaluationTableFacadePp
 from evaluation_rule_kk import EvaluationRuleKk
 from evaluation_rule_kp import EvaluationRuleKp
 from evaluation_rule_pp import EvaluationRulePp
+from evaluation_rule_mm import EvaluationRuleMm
 
 
 class EvaluationFacade():
@@ -142,7 +143,7 @@ class EvaluationFacade():
         pieces_move_as_usi_and_policy_dictionary = {}
 
         # ＫＰポリシー　ｉｎ　ＫＰテーブル
-        kp_policy_dictionary = EvaluationTableFacadeKp.make_move_as_usi_and_policy_dictionary_2(
+        kp_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
                 mm_table_obj=self._kp_table_obj,
                 a_move_collection_as_usi=king_move_collection_as_usi,
                 b_move_collection_as_usi=pieces_move_collection_as_usi,
@@ -157,12 +158,12 @@ class EvaluationFacade():
                 king_move_as_usi_and_policy_dictionary[king_move_as_usi] = policy
 
         # ＫＬポリシー　ｉｎ　ＫＫテーブル
-        kl_policy_dictionary = EvaluationTableFacadeKk.make_move_as_usi_and_policy_dictionary_2(
-                kl_table_obj=self.kk_table_obj,
-                k_move_collection_as_usi=king_move_collection_as_usi,
-                l_move_collection_as_usi=lord_move_collection_as_usi,
+        kl_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
+                mm_table_obj=self.kk_table_obj,
+                a_move_collection_as_usi=king_move_collection_as_usi,
+                b_move_collection_as_usi=lord_move_collection_as_usi,
                 turn=turn,
-                get_kl_index_by_2_moves=EvaluationRuleKk.get_kl_index_by_2_moves)
+                get_mm_index_by_2_moves=EvaluationRuleKk.get_kl_index_by_2_moves)
 
         # 評価値をマージ
         for king_move_as_usi, policy in kl_policy_dictionary.items():
@@ -172,7 +173,7 @@ class EvaluationFacade():
                 king_move_as_usi_and_policy_dictionary[king_move_as_usi] = policy
 
         # ＫＱポリシー　ｉｎ　ＫＰテーブル
-        kq_policy_dictionary = EvaluationTableFacadeKp.make_move_as_usi_and_policy_dictionary_2(
+        kq_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
                 mm_table_obj=self._kp_table_obj,
                 a_move_collection_as_usi=king_move_collection_as_usi,
                 b_move_collection_as_usi=quaffers_move_collection_as_usi,
@@ -187,12 +188,12 @@ class EvaluationFacade():
                 king_move_as_usi_and_policy_dictionary[king_move_as_usi] = policy
 
         # ＰＰポリシー　ｉｎ　ＰＰテーブル
-        pp_policy_dictionary = EvaluationTableFacadePp.make_move_as_usi_and_policy_dictionary_2(
-                pq_table_obj=self._pp_table_obj,
-                p_move_collection_as_usi=pieces_move_collection_as_usi,
-                q_move_collection_as_usi=pieces_move_collection_as_usi,
+        pp_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
+                mm_table_obj=self._pp_table_obj,
+                a_move_collection_as_usi=pieces_move_collection_as_usi,
+                b_move_collection_as_usi=pieces_move_collection_as_usi,
                 turn=turn,
-                get_pq_index_by_2_moves=EvaluationRulePp.get_pq_index_by_2_moves)
+                get_mm_index_by_2_moves=EvaluationRulePp.get_pq_index_by_2_moves)
 
         # 評価値をマージ
         for piece_move_as_usi, policy in pp_policy_dictionary.items():
@@ -203,7 +204,7 @@ class EvaluationFacade():
 
         # ＰＬポリシー　ｉｎ　ＫＰテーブル
         # TODO 盤の先後をひっくり返さないといけないか？
-        pl_policy_dictionary = EvaluationTableFacadeKp.make_move_as_usi_and_policy_dictionary_2(
+        pl_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
                 mm_table_obj=self._kp_table_obj,
                 a_move_collection_as_usi=pieces_move_collection_as_usi,
                 b_move_collection_as_usi=lord_move_collection_as_usi,
@@ -218,12 +219,12 @@ class EvaluationFacade():
                 pieces_move_as_usi_and_policy_dictionary[piece_move_as_usi] = policy
 
         # ＰＱポリシー　ｉｎ　ＰＰテーブル
-        pq_policy_dictionary = EvaluationTableFacadePp.make_move_as_usi_and_policy_dictionary_2(
-                pq_table_obj=self._pp_table_obj,
-                p_move_collection_as_usi=pieces_move_collection_as_usi,
-                q_move_collection_as_usi=quaffers_move_collection_as_usi,
+        pq_policy_dictionary = EvaluationRuleMm.make_move_as_usi_and_policy_dictionary_2(
+                mm_table_obj=self._pp_table_obj,
+                a_move_collection_as_usi=pieces_move_collection_as_usi,
+                b_move_collection_as_usi=quaffers_move_collection_as_usi,
                 turn=turn,
-                get_pq_index_by_2_moves=EvaluationRulePp.get_pq_index_by_2_moves)
+                get_mm_index_by_2_moves=EvaluationRulePp.get_pq_index_by_2_moves)
 
         # 評価値をマージ
         for piece_move_as_usi, policy in pq_policy_dictionary.items():
--- evaluation_rule_mm.py
+++ evaluation_rule_mm.py
@@ -1,43 +1,7 @@
-from evaluation_table_mm import EvaluationTableMm
-from evaluation_rule_k import EvaluationRuleK
-from evaluation_rule_kk import EvaluationRuleKk
-from evaluation_table_size_facade_kk import EvaluationTableSizeFacadeKk
 from move import Move
 
 
-class EvaluationTableFacadeKk():
-    """評価値テーブル　ＫＫ"""
-
-
-    @staticmethod
-    def create_it(
-            file_number,
-            file_name,
-            raw_mm_table,
-            is_file_modified):
-        """初期化
-
-        Parameters
-        ----------
-        is_file_modified : bool
-            保存されていない評価値テーブルを引数で渡したなら真
-        """
-
-        # テーブル・サイズ。計算過程付き
-        table_size_obj = EvaluationTableSizeFacadeKk.create_it()
-
-        k_size = EvaluationRuleK.get_king_move_number()
-        l_size = EvaluationRuleK.get_king_move_number()
-
-        return EvaluationTableMm(
-                file_number=file_number,
-                file_name=file_name,
-                table_size_obj=table_size_obj,
-                list_of_move_size=[k_size, l_size],
-                raw_mm_table=raw_mm_table,
-                is_king_size_of_a=True,
-                is_king_size_of_b=True,
-                is_file_modified=is_file_modified)
+class EvaluationRuleMm():
 
 
     @staticmethod
